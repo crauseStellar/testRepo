2cd1e43e8e260da284d4616b1cbb6fb4a84a257d
iskandar <crauseetic@gmail.com>
2023-08-28 08:54:39 +0600

    initial commit

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+public class Student extends Person{
+    private String course;
+
+    public Student(String course) {
+        this.course = course;
+    }
+
+    public Student(String name, String course) {
+        super(name);
+        this.course = course;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+//    @Override
+//    public void sayHello(){
+//        System.out.println(getName()+" from "+getCourse()+" says hi!");
+//    }
+}
