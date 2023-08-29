e4cd5f724d5a949acb3d6333ecfd952a2c58740e
stellar <stellar@gmail.com>
2023-08-29 09:20:32 +0600

    test comment

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Student("David","JavaBootCamp");
         person.sayHello();
-
+        //test comment to GitHub
     }
 }
