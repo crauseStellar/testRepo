ea2d1b1f5c122f88fae630ae19138d6baf4c9fa3
Ilimzhin <79404011+Ilimzhin@users.noreply.github.com>
2023-08-29 09:39:23 +0600

    commit from github

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Student("David","JavaBootCamp");
+        person.sayHello();
+        //test comment to GitHub
+        // comment from GITHub
+        System.out.println("say Hello world");
+        //comment for fetch
+        //comment for pull
+    }
+}
